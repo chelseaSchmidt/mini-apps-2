07ffcf25c7c0763318bea1dc7ed4da2acdf0c48d
Chelsea Schmidt <cschmidt331@gmail.com>
2020-07-29 15:26:49 -0700

    ESLint fixes

--- challenge_1/client/components/App.jsx
+++ challenge_1/client/components/App.jsx
@@ -3,6 +3,14 @@ import axios from 'axios';
 import ReactPaginate from 'react-paginate';
 import '../styles/style.css';
 
+/*
+ * Build a React UI that allows the user to search for historical events based
+ * on a keyword. Use the full-text search features of json-server to return a
+ * result to the UI for browsing. Paginate the list of events using
+ * react-paginate, loading no more than ten at a time. Ensure you are
+ * implementing server-side pagination  NOT client-side pagination.
+*/
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -25,7 +33,13 @@ export default class App extends React.Component {
   }
 
   handleSubmit() {
-    axios.get('/events')
+    axios({
+      method: 'get',
+      url: '/events',
+      params: {
+        date: '-300',
+      },
+    })
       .then(({ data }) => console.log(data))
       .catch((err) => console.error(err));
   }
@@ -47,20 +61,20 @@ export default class App extends React.Component {
         </form>
         <div id="react-paginate">
           <ReactPaginate
-            previousLabel={'previous'}
-            nextLabel={'next'}
-            breakLabel={'...'}
-            breakClassName={'break-me'}
+            previousLabel="previous"
+            nextLabel="next"
+            breakLabel="..."
+            breakClassName="break-me"
             pageCount={pageCount}
             marginPagesDisplayed={2}
             pageRangeDisplayed={5}
             onPageChange={this.handlePageClick}
-            containerClassName={'pagination'}
-            subContainerClassName={'pages pagination'}
-            activeClassName={'active'}
+            containerClassName="pagination"
+            subContainerClassName="pages pagination"
+            activeClassName="active"
           />
         </div>
       </div>
     );
   }
-};
+}
--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
   },
   mode: 'development',
   devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
